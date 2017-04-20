ec30adaad20e115dae0957d55ef05bd1fc4e3ce1
hanweimin <hanweimin@15w.com>
2017-04-20 18:20:27 +0800

    README.md提交

--- js复习/animate.js
+++ js复习/animate.js
@@ -1,3 +1,22 @@
 function animate(obj,json,fn){
+    if(obj.timer){
+        clearInterval(obj.timer);
+    }
+    obj.timer = setInterval(function () {
+        var flag = true;
+        for(var k in json){
+            if(k === "opacity"){
+                var leader = getStyle(obj,k) *100;
 
+            }
+        }
+    },15)
+}
+
+function getStyle(obj,attr){
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];
+    }else{
+        return window.getComputedStyle(obj,null)[attr];
+    }
 }
