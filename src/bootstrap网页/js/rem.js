6f7f19af662874e3c64eaa3ad141775b3ee92a7a
hanweimin <hanweimin@15w.com>
2017-06-16 19:20:59 +0800

    add

---
+++ src/bootstrap网页/js/rem.js
@@ -0,0 +1,29 @@
+/**
+ * Created by GZKJ on 2017/1/11.
+ */
+(function(win) {
+    var doc = win.document;
+    var docEl = doc.documentElement;
+    var tid;
+
+    function refreshRem() {
+        var width = docEl.getBoundingClientRect().width;
+        console.log(width);
+        var rem = width / 37.5; // 将屏幕宽度分成10份， 1份为1rem  
+        docEl.style.fontSize = rem + 'px';
+            console.log(rem);
+
+    }
+    win.addEventListener('resize', function() {
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 100);
+    }, false);
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 100);
+        }
+    }, false);
+    refreshRem();
+})(window);
+
